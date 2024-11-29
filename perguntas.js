d51dd90351b0aece5ec4b684ca597d9bc6ba898e
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-11-29 17:21:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'carro',
+    'O que é um v8?',
+    'Um V8 é um tipo de motor de carro com oito cilindros dispostos em forma de "V". Ele é conhecido por ser potente e usado em carros mais rápidos e fortes.'
+)
+
+criaCartao(
+    'Carro',
+    'Qual o carro mais rapido do mundo?',
+    'O carro mais rápido do mundo é o Bugatti Chiron Super Sport 300+, que alcançou 490 km/h. Outros carros rápidos incluem o SSC Tuatara e o Koenigsegg Jesko Absolut.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
